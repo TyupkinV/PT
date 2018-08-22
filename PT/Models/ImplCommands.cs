5f1847cdcdc0d5e45301c141f0be6e7f3dc5f4fc
TyupkinV <Tyupkin.V@gmail.com>
2018-08-22 17:26:55 +0300

    Implement ICommand interface

---
+++ PT/Models/ImplCommands.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+
+
+namespace PT.Models {
+    class ImplCommands : ICommand {
+        private Action<object> _execute;
+        private Predicate<object> _canExecute;
+
+        public event EventHandler CanExecuteChanged {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public ImplCommands(Predicate<object> canExec, Action<object> exec) {
+            _execute = exec;
+            _canExecute = canExec;
+        }
+
+        public ImplCommands(Action<object> exec) : this(null, exec) {
+        }
+
+        public bool CanExecute(object parameter) {
+            return _canExecute(parameter);
+        }
+
+        public void Execute(object parameter) {
+            _execute(parameter);
+        }
+    }
+}
