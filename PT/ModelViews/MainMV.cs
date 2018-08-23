94dd6c16a267cc0336bb327d9f971724ab03d475
TyupkinV <Tyupkin.V@gmail.com>
2018-08-23 18:04:10 +0300

    Implemented search all paths

--- PT/ModelViews/MainMV.cs
+++ PT/ModelViews/MainMV.cs
@@ -1,17 +1,45 @@
 ﻿using System;
 using System.Windows.Input;
 using PT.Models;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace PT.ModelViews {
-    public class MainMV {
+    public class MainMV : INotifyPropertyChanged{
 
         public LoadDataM LoadDataMProp { get; private set; }
         public ICommand CommLoadFile { get; private set; }
+        public ICommand CommFindPaths { get; private set; }
+        private string _startPoint = "1";
+        public string StartPoint { get { return _startPoint; } set {_startPoint = value; NotifyPropertyChanged(); } }
+        private string _endPoint = "4";
+        public string EndPoint { get { return _endPoint; } set { _endPoint = value; NotifyPropertyChanged(); } }
+
 
         public MainMV() {
-            LoadDataMProp = new LoadDataM();
+            LoadDataMProp = new LoadDataM(this);
             CommLoadFile = new ImplCommands(ReadFile);
+            CommFindPaths = new ImplCommands(CheckFields, FindPath);
 
+        }
+
+        private bool CheckFields(object obj) {
+            if(StartPoint == string.Empty || EndPoint == string.Empty) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+
+        private void FindPath(object obj) {
+            LoadDataMProp.MakeGraph();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName]string prop = "") {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
         }
 
         private void ReadFile(object obj) {
--- PT/Models/LoadDataM.cs
+++ PT/Models/LoadDataM.cs
@@ -4,8 +4,17 @@ using System.Linq;
 using System.IO;
 using Microsoft.Win32;
 
+
 namespace PT.Models {
     public class LoadDataM {
+
+        public ModelViews.MainMV MainMVP { get; set; }
+        public List<Bus> AllBus { get; set; } = new List<Bus>();
+        public List<List<Tuple<int, int>>> AllPaths { get; set; } = new List<List<Tuple<int, int>>>();
+
+        public LoadDataM(ModelViews.MainMV mainMV) {
+            MainMVP = mainMV;
+        }
 
         public void ReadFile() {
             OpenFileDialog openFileOFD = new OpenFileDialog {
@@ -16,42 +25,83 @@ namespace PT.Models {
             if (openFileOFD.ShowDialog() == true) {
                 StreamReader reader = new StreamReader(openFileOFD.OpenFile());
                 int sumBus = Convert.ToInt32(reader.ReadLine());
-                List<Bus> listBus = new List<Bus>();
-                while (listBus.Count < sumBus) {
-                    listBus.Add(new Bus());
+                
+                while (AllBus.Count < sumBus) {
+                    AllBus.Add(new Bus());
                 }
 
                 CommonInfo.SumPoint = Convert.ToInt32(reader.ReadLine());
 
                 string[] timesStart = reader.ReadLine().Split(' ');
                 for(int i = 0; i < timesStart.Length; i++) { 
-                     listBus[i].StartWaybill = DateTime.Parse(timesStart[i]);
+                     AllBus[i].StartWaybill = DateTime.Parse(timesStart[i]);
                 }
 
                 var prices = from i in reader.ReadLine().Split(' ')
                              select Convert.ToInt32(i);
                 prices = prices.ToList();
                 for (int j = 0; j < prices.Count(); j++) {
-                    listBus[j].Price = prices.ElementAt(j);
+                    AllBus[j].Price = prices.ElementAt(j);
                 }
 
-                foreach(Bus bus in listBus) {
+                foreach(Bus bus in AllBus) {
                     string[] line = reader.ReadLine().Split(' ');
                     int iter = Convert.ToInt32(line[0]);
-                    Dictionary<string, int> tempWaybill = new Dictionary<string, int>();
+                    Dictionary<int, int> tempWaybill = new Dictionary<int, int>();
                     for(int i = 1; i <= iter; i++) {
-                        tempWaybill.Add(line[i], Convert.ToInt32(line[i + iter]));
+                        tempWaybill.Add(Convert.ToInt32(line[i]) - 1, Convert.ToInt32(line[i + iter]));
                     }
                     bus.Waybill = tempWaybill;
-                    int b = 0;
                 }
             } 
         }
 
-        class Bus {
+        public void MakeGraph() {
+            IList<int>[,] graph = new IList<int>[CommonInfo.SumPoint, CommonInfo.SumPoint];
+            for(int i = 0; i < CommonInfo.SumPoint; i++) {
+                for (int j = 0; j < CommonInfo.SumPoint; j++) {
+                    graph[i, j] = new List<int>();
+                }
+            }
+
+            foreach(Bus bus in AllBus) {
+                List<int> allPoint = bus.Waybill.Keys.ToList();
+                graph[allPoint[allPoint.Count - 1], allPoint[0]].Add(bus.IDBus);
+                for(int i = 0; i < allPoint.Count - 1; i++) {
+                    graph[allPoint[i], allPoint[i + 1]].Add(bus.IDBus);
+                }
+            }
+
+            AllPath(graph, Convert.ToInt32(MainMVP.StartPoint) - 1, Convert.ToInt32(MainMVP.EndPoint) - 1, new List<Tuple<int, int>>(), Enumerable.Repeat(false, CommonInfo.SumPoint).ToList());
+
+        }
+
+        private void AllPath(IList<int>[,] graph, int startPoint, int arrivalPoint, List<Tuple<int, int>> path, List<bool> visited) {
+            visited[startPoint] = true;
+            if (startPoint == arrivalPoint) {
+                AllPaths.Add(path);
+                path.Clear();
+                visited[arrivalPoint] = false;
+                return;
+            }
+            for (int nextPoint = 0; nextPoint < CommonInfo.SumPoint; nextPoint++) {
+                if ( !visited[nextPoint] && graph[startPoint, nextPoint].Count != 0) {
+                    foreach (int nextBus in graph[startPoint, nextPoint]) {
+                        path.Add(new Tuple<int, int>(nextBus, nextPoint));
+                        Console.WriteLine("{0}-{1}", startPoint, nextPoint);
+                        AllPath(graph, nextPoint, arrivalPoint, path, visited);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+    public class Bus {
             static public int CounterBus = 0;
             public int IDBus { get; set; }
-            public Dictionary<string, int> Waybill { get; set; }
+            public Dictionary<int, int> Waybill { get; set; }
             public DateTime StartWaybill { get; set; }
             public int Price { get; set; }
             public string NextStay { get; set; }
@@ -66,16 +116,16 @@ namespace PT.Models {
             }
         }
 
-        static class CommonInfo {
+    static class CommonInfo {
             static public DateTime Time { get; set; } = new DateTime();
             static public int SumPoint { get; set; } = 0;
             static public int SumBus { get; set; } = 0;
         }
 
-        class Passenger {
+    class Passenger {
             public DateTime StartTrip { get; set; }
             public string StartPoint { get; set; }
             public string FinishPoint { get; set; }
         }
-    }
-}
+
+
