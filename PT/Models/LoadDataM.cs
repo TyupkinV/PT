1a96b1b51b0d87a5a55af4d3ae041d07be36ef9c
TyupkinV <Tyupkin.V@gmail.com>
2018-08-25 14:11:32 +0300

    Revert "Alternative storage graph (list adjesty) and change method search paths"
    This reverts commit 017f97e07af29ae475dbadf4be2e884115076e5d.

--- PT/Models/LoadDataM.cs
+++ PT/Models/LoadDataM.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 using Microsoft.Win32;
-using System.Windows;
 
 
 namespace PT.Models {
@@ -12,7 +11,6 @@ namespace PT.Models {
         public ModelViews.MainMV MainMVP { get; set; }
         public List<Bus> AllBus { get; set; } = new List<Bus>();
         public List<List<Tuple<int, int>>> AllPaths { get; set; } = new List<List<Tuple<int, int>>>();
-        public int StartPointProp { get; set; }
 
         public LoadDataM(ModelViews.MainMV mainMV) {
             MainMVP = mainMV;
@@ -59,91 +57,75 @@ namespace PT.Models {
         }
 
         public void MakeGraph() {
-            List<Edge> listAdj = new List<Edge>();
-                foreach(Bus bus in AllBus) {
-                    List<int> allPoint = bus.Waybill.Keys.ToList();
-                    listAdj.Add(new Edge { StartV = allPoint[allPoint.Count - 1], EndV = allPoint[0], IDBus = bus.IDBus });
-                    for(int i = 0; i < allPoint.Count - 1; i++) {
-                        listAdj.Add(new Edge { StartV = allPoint[i], EndV = allPoint[i + 1], IDBus = bus.IDBus, Visited = false });
-                    }
+            IList<int>[,] graph = new IList<int>[CommonInfo.SumPoint, CommonInfo.SumPoint];
+            for(int i = 0; i < CommonInfo.SumPoint; i++) {
+                for (int j = 0; j < CommonInfo.SumPoint; j++) {
+                    graph[i, j] = new List<int>();
                 }
+            }
 
-            StartPointProp = Convert.ToInt32(MainMVP.StartPoint) - 1;
-            AllPath(listAdj, StartPointProp, Convert.ToInt32(MainMVP.EndPoint) - 1, new List<Tuple<int, int>>());
-            if(AllPaths.Count != 0) {
-                BudgetPath();
+            foreach(Bus bus in AllBus) {
+                List<int> allPoint = bus.Waybill.Keys.ToList();
+                graph[allPoint[allPoint.Count - 1], allPoint[0]].Add(bus.IDBus);
+                for(int i = 0; i < allPoint.Count - 1; i++) {
+                    graph[allPoint[i], allPoint[i + 1]].Add(bus.IDBus);
+                }
             }
-            else {
-                MessageBox.Show("Маршрутов не найдено.");
-            }
-            
+
+            AllPath(graph, Convert.ToInt32(MainMVP.StartPoint) - 1, Convert.ToInt32(MainMVP.EndPoint) - 1, new List<Tuple<int, int>>(), Enumerable.Repeat(false, CommonInfo.SumPoint).ToList());
+
         }
 
-        private void AllPath(List<Edge> graph, int startPoint, int arrivalPoint, List<Tuple<int, int>> path) {
-            graph[startPoint].Visited = true;
+        private void AllPath(IList<int>[,] graph, int startPoint, int arrivalPoint, List<Tuple<int, int>> path, List<bool> visited) {
+            visited[startPoint] = true;
             if (startPoint == arrivalPoint) {
-                AllPaths.Add(new List<Tuple<int, int>>(path));
-                Console.WriteLine("-");
+                AllPaths.Add(path);
                 path.Clear();
-                graph[startPoint].Visited = false;
+                visited[arrivalPoint] = false;
                 return;
             }
-            foreach (Edge edge in graph) {
-                if (edge.StartV == startPoint && !edge.Visited && edge.EndV != StartPointProp) {
-                    path.Add(new Tuple<int, int>(edge.IDBus, edge.EndV));
-                    Console.WriteLine("{0}-{1}", startPoint, edge.EndV);
-                    AllPath(graph, edge.EndV, arrivalPoint, new List<Tuple<int, int>>(path));
-                    path.RemoveAt(path.Count - 1);
+            for (int nextPoint = 0; nextPoint < CommonInfo.SumPoint; nextPoint++) {
+                if ( !visited[nextPoint] && graph[startPoint, nextPoint].Count != 0) {
+                    foreach (int nextBus in graph[startPoint, nextPoint]) {
+                        path.Add(new Tuple<int, int>(nextBus, nextPoint));
+                        Console.WriteLine("{0}-{1}", startPoint, nextPoint);
+                        AllPath(graph, nextPoint, arrivalPoint, path, visited);
+                    }
                 }
             }
         }
-        private void BudgetPath() {
-            int b = 0;
+    }
+}
+
+
+    public class Bus {
+            static public int CounterBus = 0;
+            public int IDBus { get; set; }
+            public Dictionary<int, int> Waybill { get; set; }
+            public DateTime StartWaybill { get; set; }
+            public int Price { get; set; }
+            public string NextStay { get; set; }
+            public int TimeToNextStay { get; set; } = 0;
+
+            public Bus() {
+                IDBus = ++CounterBus;
+            }
+
+            public string GetNextStay() {
+                return new NotImplementedException().ToString();
+            }
         }
-    }
-}
-  
 
+    static class CommonInfo {
+            static public DateTime Time { get; set; } = new DateTime();
+            static public int SumPoint { get; set; } = 0;
+            static public int SumBus { get; set; } = 0;
+        }
 
-
-
-
-public class Bus {
-    static public int CounterBus = 0;
-    public int IDBus { get; set; }
-    public Dictionary<int, int> Waybill { get; set; }
-    public DateTime StartWaybill { get; set; }
-    public int Price { get; set; }
-    public string NextStay { get; set; }
-    public int TimeToNextStay { get; set; } = 0;
-
-    public Bus() {
-        IDBus = ++CounterBus;
-    }
-
-    public string GetNextStay() {
-        return new NotImplementedException().ToString();
-    }
-}
-
-static class CommonInfo {
-    static public DateTime Time { get; set; } = new DateTime();
-    static public int SumPoint { get; set; } = 0;
-    static public int SumBus { get; set; } = 0;
-}
-
-class Passenger {
-    public DateTime StartTrip { get; set; }
-    public string StartPoint { get; set; }
-    public string FinishPoint { get; set; }
-}
-
-class Edge {
-    public int StartV { get; set; }
-    public int EndV { get; set; }
-    public int IDBus { get; set; }
-    public bool Visited { get; set; }
-}
-
+    class Passenger {
+            public DateTime StartTrip { get; set; }
+            public string StartPoint { get; set; }
+            public string FinishPoint { get; set; }
+        }
 
 
